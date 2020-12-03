91075cfc110cb992528bb9a83540eb6b42933e11
Sahil Bhatia <sahilofficial671@gmail.com>
2020-12-03 18:32:14 +0530

    load event js added

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,12 @@
-$(document).ready(function() {
+
+$(document).load(function(){
     if($(window).width() < 600){
         $('.sidebar').css('display', 'none');
         $('#navbarSupportedContent').addClass('hidden');
     }
+});
+$(document).ready(function() {
+
     $('nav button').on('click', function() {
         var sidebar = $('.sidebar');
         if($(window).width() < 600){
