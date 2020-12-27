c7bd7c1857c26bd9918f0065e542ad7640391644
Sahil Bhatia <sahilofficial671@gmail.com>
2020-12-27 19:41:41 +0530

    Just a change

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -40,7 +40,6 @@ class Contact extends Model
     {
         return $this->belongsTo('App\Models\Account', 'account_id', 'id');
     }
-
     public function company()
     {
         return $this->hasOne('App\Models\Company', 'id', 'company_id');
